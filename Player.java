71111fe55999e566f6eb84c7a8df926698cc2d4d
Shaun-Aurelius Pinzon <shaunpinzonb@gmail.com>
2023-03-05 22:49:15 -0800

    Add files via upload

--- Player.java
+++ Player.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class Player {
     private String name;
-    private Integer dirt;
-    private Integer smallGrowthToken;
-    private Integer largeGrowthToken;
-    private Integer vp;
+    private Integer dirt = 0;
+    private Integer smallGrowthToken = 0;
+    private Integer largeGrowthToken = 0;
+    private Integer compost = 0;
+    private Integer vp = 0;
 
     private List<Island_Card> hand;
 
@@ -44,7 +45,16 @@ public class Player {
         this.largeGrowthToken = LGT + this.largeGrowthToken;
     }
 
+    public void addCompost(Integer Compost){
+        this.compost = Compost + this.compost;
+    }
     public void addVP(Integer VP){
         this.vp = vp + this.vp;
     }
+
+    public int getDirt(){return dirt; }
+    public int getSmallGrowthToken(){return smallGrowthToken; }
+    public int getLargeGrowthToken(){return largeGrowthToken; }
+    public int getCompost(){return compost; }
+    public int getVP(){return vp; }
 }
