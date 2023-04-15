ff5946344d18582259bf1ae060e0a809342beb99
Shaun-Aurelius Pinzon <shaunpinzonb@gmail.com>
2023-04-14 23:26:58 -0700

    Player Board

--- src/main/java/Board/Player_Board.java
+++ src/main/java/Board/Player_Board.java
@@ -1,5 +1,15 @@
 package Board;
 
 public class Player_Board {
+	private Island_Card islandCard;
+	private Ecosystem_Card ecosystemCard;
+	private Climate_Card climateCard;
+
+	public Player_Board(Island_Card islandCard, Ecosystem_Card ecosystemCard, Climate_Card climateCard){
+		this.islandCard = islandCard;
+		this.ecosystemCard = ecosystemCard;
+		this.climateCard = climateCard;
+	}
+
 
 }
